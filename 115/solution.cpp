513f4ff6c9d5b888b2cbe0e149f31d280f275d1b
rfliu6 <rfliu6@gmail.com>
2017-02-27 14:38:24 +0800

    Create solution.cpp

---
+++ 115/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int m = t.size(), n = s.size();
+        vector<vector<int>> dp(m+1,vector<int>(n+1,0));    // dp[i][j]   s[j-1] t[i-1]
+        for(int i=0; i<=n; i++)   // empty string is the only subsequence of any string
+            dp[0][i] = 1;
+
+        for(int i=1; i<=m; i++)
+            for(int j=1; j<=n; j++){
+                if(s[j-1] == t[i-1])
+                    dp[i][j] = dp[i-1][j-1] + dp[i][j-1];
+                else
+                    dp[i][j] = dp[i][j-1];
+            }
+        return dp[m][n];
+    }
+};
